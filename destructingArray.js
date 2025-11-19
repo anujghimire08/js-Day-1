1f3fde69bc87937b888117a959e588b3c4426a56
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-19 13:58:21 +0545

    Add examples of array destructuring with console output

---
+++ destructingArray.js
@@ -0,0 +1,18 @@
+let details = ["John", 30, "Engineer"];
+let [name, age, profession] = details;
+console.log(name); // Output: John
+console.log(age); // Output: 30
+console.log(profession); // Output: Engineer
+
+let colors = ["Red", "Green", "Blue", "Yellow"];
+let [firstColor, secondColor, thirdColors] = colors;
+console.log(firstColor);
+console.log(secondColor);
+console.log(thirdColors);
+// Output:
+// Red
+// Green
+// Blue
+
+//Destructuring an array means unpacking values from an array into individual variables.
+ 

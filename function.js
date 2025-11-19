5aa924067402e793c941796f3caac10160964c90
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-19 13:31:43 +0545

    Add password length check function with console output

--- function.js
+++ function.js
@@ -4,3 +4,15 @@ function increment(num) {
 console.log("The final value is: " + increment(5));
 // Output:
 // The final value is: 5
+
+function passwordLengthCheck(length) {
+  if (length >= 8) {
+    console.log("Length is sufficient");
+  } else {
+    console.log("Length is insufficient");
+  }
+}
+let password = "p@$$word124";
+passwordLengthCheck(password.length);
+// Output:
+// Length is sufficient

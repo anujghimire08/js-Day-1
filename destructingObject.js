f8d5ef3dc23634bbc1c30cf3b4855573acc14aae
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-19 14:09:00 +0545

    Add object destructuring example with console output

---
+++ destructingObject.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+
+let { name, age, city } = person;
+console.log(name); // Output: John
+console.log(age); // Output: 30
+console.log(city); // Output: New York
+
+//Destructuring an object means extracting values from an object and storing them in variables easily.

f97e9d5cd541bba507b7a0cf625d13309e997f95
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-19 13:42:23 +0545

    Add array and object examples with console output

---
+++ Array.js
@@ -0,0 +1,4 @@
+let arr = [10, 20, 30, 40, 50];
+console.log(arr[2]);
+
+//An array is a list-like structure used to store multiple values in one variable.
---
+++ object.js
@@ -0,0 +1,14 @@
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+console.log(person.city);
+
+const employee = [
+  { id: 1, nam: "hari", position: "developer" },
+  { id: 2, nam: "sita", position: "designer" },
+];
+console.log(employee[1].nam);
+
+//An object is a collection of key-value pairs used to store related data.
